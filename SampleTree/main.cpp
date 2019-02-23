e8efaeb8695b9223d1389999073b14f611a27049
AndrewAlmighty <xaidianx@gmail.com>
2019-02-23 13:01:51 +0100

    Update main.cpp

--- SampleTree/main.cpp
+++ SampleTree/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 
     TreeNode<int> a1(1, "Root");
     TreeNode<int>* ptr = nullptr;
-    a1.addChild(std::make_shared<TreeNode<int>>(1));
+    a1.addChild(std::make_shared<TreeNode<int>>(10));
     a1.addChild(std::make_shared<TreeNode<int>>(2));
     a1.addChild(std::make_shared<TreeNode<int>>(3));
 
@@ -29,11 +29,20 @@ int main(int argc, char *argv[])
 
     ptr = a1.getChild(1);
     ptr -> addChild(std::make_shared<TreeNode<int>>(6));
+    ptr -> addChild(std::make_shared<TreeNode<int>>(7));
+    ptr -> addChild(std::make_shared<TreeNode<int>>(7));
 
     ptr = a1.getChild(0);
 
     ptr = ptr -> getChild(0);
-    ptr -> addChild(std::make_shared<TreeNode<int>>(7));
+    ptr -> addChild(std::make_shared<TreeNode<int>>(8));
+    ptr -> addChild(std::make_shared<TreeNode<int>>(9));
+
+    ptr = ptr -> getChild(1);
+    ptr -> addChild(std::make_shared<TreeNode<int>>(10));
+
+    std::cout << "Prepared tree. Serialized tree looks like:\n" << ptr -> serializeTree() << std::endl;
+    std::cout << "Rendering tree.";
 
     QApplication a(argc, argv);
     MainWindow w;
